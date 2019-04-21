35a813fd370ac0d30c4e6d8dc2a6b792348f4fe3
Unknown <rparsons01@gmail.com>
2019-04-22 00:12:28 +1000

    temp_check() added and no_load_speed_test() mod to include max speed

--- Odrive script/Dyno_data_logger_v1.1.py
+++ Odrive script/Dyno_data_logger_v1.1.py
@@ -23,15 +23,13 @@ Run python script with motor controllers connected.
 
 TO DO:  * Set-up second odrive and test motor readings.
         * Add actual readings of the motor thermistors rather than just place holders
-        * Add checks for motor and board temps to make sure they don't get too hot.
-        * Add motor parameters at start of file (motor resistance, inductance etc.)
-        
+        * Add checks for motor and board temps to make sure they don't get too hot.        
 """
 
 #output data text file set-up
 text_file_name              = 'odrive_loss.csv' # Name of output text file
 file_header_flag            = True 	# Set False to exclude the file header (name and unit at start of file)
-erase_file_on_startup_flag  = True 	# Set True to erase text file at the start of each run or between tests
+erase_file_on_startup_flag  = False # Set True to erase text file at the start of each run or between tests
 
 #logging parameters
 stabilise_time              = 0.5   # Time elapsed after setting parameters before measuring values [s]
@@ -46,13 +44,15 @@ encoder_cpr                 = 8192  # Encoder counts per rotation [CPR]
 test_brake_resistance       = 0.47  # resistance of the brake resistor connected to test motor odrive[Ohm]
 absorber_brake_resistance   = 2.0   # resistance of the brake resistor connected to the absorber odrive[Ohm]
 shutdown_ramp_speed         = 10E4 	# Speed to slow down at end of test [counts/s/s]
+max_safe_temp               = 80   # Maximum allowable temperature for motor and MOSFETs [Deg C]
 
 # Motor parameter reporting
 text_file_name_motor_param	= 'Motor_parameter.csv' # Name of output text file
 
 # No-load speed test
-no_load_num_steps           = 70    # Number of steps to increment RPM and take a reading
+no_load_max_motor_speed     = 7000  # Maximum safe speed for motor. Note: Check encoder maximum RPM settings [RPM]
 no_load_speed_step          = 100   # Step size between readings [RPM]
+no_load_vel_error           = 10    # Maximum allowable error between true motor speed and commanded speed [%]
 
 # Motor controller power loss estimation
 loss_est_num_steps			= 14	# Number of steps to increment motor current and take a reading
@@ -151,6 +151,25 @@ measurement_list = [
 }
 ]
 
+def rpm_to_cpr(x):
+    
+    """
+    Converts motor RPM to counts/s for use by motor controller.
+    """
+    return (x / 60)* encoder_cpr;
+
+def temp_check()
+    """
+    Compares motor and MOSFET temperatures to max_safe_temp.
+    If exceeded, measurement paused until safe temp reached and an error printed to console.
+    """
+    while my_odrive.axis0.muv3 >= max_safe_temp >= or my_odrive.axis0.motor.get_inverter_temp() >= max_safe_temp:
+        time.sleep(5)
+        print('Warning: Maximum safe temperature (', max_safe_temp ' Deg C) exceeded.')
+        print('Motor Temperature (Deg C): ', my_odrive.axis0.muv3 )
+        print('MOSFET Temperature (Deg C): ', my_odrive.axis0.motor.get_inverter_temp())
+    return
+
 def measure_values():
     """
     Measures the values outlined in measurement_list.
@@ -227,6 +246,7 @@ def odriveStartup():
     my_odrive = odrive.find_any()
     print("odrive found...")
     my_odrive.config.brake_resistance = absorber_brake_resistance
+    temp_check() # Check all temperatures are safe.
 
     # Find an ODrive that is connected on the serial port /dev/ttyUSB0
     #my_drive = odrive.find_any("serial:/dev/ttyUSB0")
@@ -262,19 +282,13 @@ def odriveShutdown():
         # Calibrate motor and wait for it to finish
         my_odrive.axis0.controller.config.control_mode = 1
 
-def rpm_to_cpr(x):
-    
-    """
-    Converts motor RPM to counts/s for use by motor controller.
-    """
-    return (x / 60)* encoder_cpr;
-
 def report_motor_parameters():
 
 	"""
 	Tests motor resistance and inductance and prints it to a separate text file.
 	"""
 	print("Testing motors...")
+    temp_check() # Check all temperatures are safe.
 	my_odrive.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
     while my_odrive.axis0.current_state != AXIS_STATE_IDLE:
         time.sleep(0.1)
@@ -299,32 +313,42 @@ def report_motor_parameters():
 def no_load_speed_test():
     
     """
-    Sets motor to velocity control mode.
-    Increments speed by speed_step up to no_load_num_steps * no_load_speed_step.
-    Logs data to text file at each speed.
+    Estimates maximum motor speed and motor + motor controller no-load power loss.
+    Increments motor speed until allowable_speed_error is exceeded.
+    Logs data for each no_load_speed_step
     """
-    my_odrive.axis0.controller.config.control_mode = 2  #set to velocity control mode
-    print("now in velocity control mode")
-    print("Starting no-load speed test")
+    my_odrive.axis0.controller.config.vel_limit = rpm_to_cpr(no_load_max_motor_speed) # Limit motor top speed [counts/s]
+    my_odrive.axis0.controller.config.control_mode = 2  # set to velocity control mode
+    my_odrive.axis0.controller.vel_setpoint = 0 # Stop motor if it isn't already stopped [counts/s]
+        
+    while my_odrive.axis0.encoder.vel_estimate != 0:# wait for motors to slow down.
+        time.sleep(0.1)
 
-    for i in range(no_load_num_steps):
-        max_speed = (no_load_num_steps * no_load_speed_step) # First step is zero.
-        my_odrive.axis0.controller.config.vel_limit = rpm_to_cpr(max_speed) # [counts/s]
-        set_speed = i * no_load_speed_step
-        print(i, ' ', set_speed, ' ' , my_odrive.axis0.muv2)
+    print("Starting no-load speed test")
+    vel_error = 0.0
+    i = 0
+
+    # Increment speed by no_load_speed_step until
+    while 100 - no_load_vel_error <  vel_error < 100 + no_load_vel_error:
+        temp_check() # Check all temperatures are safe.
+        set_speed = i * no_load_speed_step # [counts/s]
         my_odrive.axis0.controller.vel_setpoint = rpm_to_cpr(set_speed) # [counts/s]
         time.sleep(stabilise_time) # wait for speed to stabilise [s]
+        vel_error = odrv0.axis0.encoder.vel_estimate / my_odrive.axis0.controller.vel_setpoint * 100 # speed error [%]
+        print(i, ' ', set_speed, ' ' , vel_error, ' ', my_odrive.axis0.muv2)
         write_values(measure_values())
 
     print("No-load speed test complete")
+    print("Maximum motor speed = ", set_speed)
 
 def motor_controller_loss_test():
     """
     Estimates Odrive I^2R losses by incrementally increasing motor current.
-    Motor phase current and reported resistance report_motor_parameters() gives motor power draw.
+    Motor phase current and reported motor resistance report_motor_parameters() gives motor power draw.
     Subtracting motor power draw from Odrive power draw (current shunt reading x Vbus) gives Odrive I^2R loss.
     """
     for i in range(loss_est_num_steps)
+        temp_check() # Check all temperatures are safe.
     	my_odrive.axis0.motor.config.current_lim = i * loss_est_current_step
         # Offset calibration used as a means for the motor to draw a large current without doing mechanical work.
         # Offset calibration also loads each phases evenly, resulting in even MOSFET and motor phase heating.
@@ -344,5 +368,5 @@ def motor_controller_loss_test():
 odriveStartup()
 report_motor_parameters()
 motor_controller_loss_test()
-#no_load_speed_test()
+no_load_speed_test()
 odriveShutdown()
